aa2fdb02f641bff3ccf35a30c02d4fec9de6cf16
Michael Bethke <gibsonbethke@gmail.com>
2021-11-04 20:59:34 -0500

    Debug messages received

--- src/include/networking.h
+++ src/include/networking.h
@@ -746,6 +746,8 @@ void Networking::processNetwork() {
 		}
 
 		if(messageReceivedCount > MAX_MESSAGE_RECEIVED_COUNT) {
+			Serial.print(F("count of messages received: "));
+			Serial.println(messagesReceivedCount);
 			DebugLog::getLog().logError(NETWORK_TOO_MANY_MESSAGES_RECEIVED);
 			Serial.println(F("wow sombodys popular"));
 			dropConnection();
