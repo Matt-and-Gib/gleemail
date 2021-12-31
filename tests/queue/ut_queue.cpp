5192fc1f3ae221bc81fae2a3004be83753d2ed89
AVividLight <gibsonbethke@gmail.com>
2021-12-31 12:14:55 -0600

    Fixed CMake Errors
    Not sure if this works/does anything yet, though

--- tests/queue/ut_queue.cpp
+++ tests/queue/ut_queue.cpp
@@ -1,7 +1,8 @@
-#include "../../../src/include/queue.h"
-
+#include "queue.h"
+#include <iostream>
 
 int main(int argc, char* argv[]) {
-	
+	std::cout << "ut_queue.cpp" << std::endl;
+
 	return 0;
 }
