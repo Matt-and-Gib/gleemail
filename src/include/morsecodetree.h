26ea2e41659853a0ed911390c27d89091227afee
DualJustice <mattatgbsoftware@gmail.com>
2022-01-21 07:28:51 -0600

    Minor cleanup and optimizations to morsecodetree
    Removed unnecessary comments, added some safety measures, and removed some unnecessary checks.

--- src/include/morsecodetree.h
+++ src/include/morsecodetree.h
@@ -30,10 +30,12 @@ Phrase length of 6, depth of 6. Root has a depth of 0.
 namespace GLEEMAIL_MORSE_CODE {
 	class MorsePhraseCharPair {
 	public:
-		char binaryPhrase = 0b00000000; //We could make this equal to 0b10000000 and then overwrite the most significant bit with a 0 when constructing as a sort of safety check.
+		static constexpr unsigned short MAX_MORSE_PHRASE_LENGTH = 6; //Copied from the old version. Could be a maximum of 8 before we would need to switch data types.
+
+		char binaryPhrase = 0b00000000; //A binary representation of a morse phrase, in little endian.
 		const char symbol;
 
-		MorsePhraseCharPair(const char*, const char*, const char);
+		MorsePhraseCharPair(const char*, const char*);
 
 		bool operator==(const MorsePhraseCharPair& rhs) const {
 			//todo
@@ -45,14 +47,18 @@ namespace GLEEMAIL_MORSE_CODE {
 	};
 
 
-	MorsePhraseCharPair::MorsePhraseCharPair(const char* letter, const char* phrase, const char length) : symbol{letter[0]} { //Added length parameter because it makes the conversion to binary easier. Not necessary!
-		binaryPhrase = 0b00000000; //Is this redundant? Really need to make sure it is initialized as 0!
+	MorsePhraseCharPair::MorsePhraseCharPair(const char* letter, const char* phrase) : symbol{letter[0]} {
+		char length = 0;
+		while((phrase[length] != '\0') && (length < (MAX_MORSE_PHRASE_LENGTH + 1))) {
+			length += 1;
+		}
 
+		binaryPhrase = 0b00000000; //Redundant, but safe.
 		for(char i = 0; i < length; i += 1) {
 			if(phrase[(length - 1) - i] == '.') {
-				binaryPhrase <<= 1; //Magic number simply represents one bit shift.
-			} else { //fragile else statement.
-				binaryPhrase = (binaryPhrase << 1) | 0b00000001; //Magic number simply represents the ones bit.
+				binaryPhrase <<= 1;
+			} else {
+				binaryPhrase = (binaryPhrase << 1) | 0b00000001;
 			}
 		}
 	}
@@ -60,7 +66,6 @@ namespace GLEEMAIL_MORSE_CODE {
 
 	class MorseCodeTreeNode final : public BinarySearchTreeNode<MorsePhraseCharPair> {
 	private:
-		char calculatePhraseLength(const char*);
 		//BinarySearchTreeNode* addNode(BinarySearchTreeNode*) = delete;
 	public:
 		MorseCodeTreeNode(const MorsePhraseCharPair& d) : BinarySearchTreeNode(d) {}
@@ -68,39 +73,17 @@ namespace GLEEMAIL_MORSE_CODE {
 	};
 
 
-	char MorseCodeTreeNode::calculatePhraseLength(const char* phrase) {
-		char i = 0;
-
-		while(phrase[i] != '\0') { //This could lead to problems if there is no null terminator! a.k.a it is fragile.
-			i += 1;
-		}
-
-		return i;
-	}
-
-
-	BinarySearchTreeNode<MorsePhraseCharPair>* MorseCodeTreeNode::addNode(const char* letter, const char* phrase) {
+	BinarySearchTreeNode<MorsePhraseCharPair>* MorseCodeTreeNode::addNode(const char* letter, const char* phrase) { //This function requires that nodes be added in a certain order!
 		if(!letter || !phrase) {
 			return nullptr;
 		}
 
-		char phraseLength = calculatePhraseLength(phrase); //This will need to be calculated eventually. May as well do it here as it speeds future processes up. Can be moved or even removed.
-
-		MorsePhraseCharPair* newPair = new MorsePhraseCharPair(letter, phrase, phraseLength); //We need to know the length of the phrase after this point!
-		BinarySearchTreeNode* newNode = new MorseCodeTreeNode(*newPair);
+		BinarySearchTreeNode* newNode = new MorseCodeTreeNode(new MorsePhraseCharPair(letter, phrase));
 
 		BinarySearchTreeNode* currentNode = this;
 		char depth = 0;
 		while(currentNode != nullptr) {
-			if((((newNode->getData().binaryPhrase) >> depth) & 0b00000001) == 0b00000000) { //This is where the realization that we really never compare anything becomes handy. '.' means left, '-' means right.
-				if(currentNode->getLesserChild()) {
-					currentNode = currentNode->getLesserChild();
-					depth += 1;
-				} else {
-					currentNode->setLesserChild(*newNode);
-					return newNode;
-				}
-			} else {
+			if(((newNode->getData().binaryPhrase) >> depth) & 0b00000001) {
 				if(currentNode->getGreaterChild()) {
 					currentNode = currentNode->getGreaterChild();
 					depth += 1;
@@ -108,8 +91,18 @@ namespace GLEEMAIL_MORSE_CODE {
 					currentNode->setGreaterChild(*newNode);
 					return newNode;
 				}
+			} else {
+				if(currentNode->getLesserChild()) {
+					currentNode = currentNode->getLesserChild();
+					depth += 1;
+				} else {
+					currentNode->setLesserChild(*newNode);
+					return newNode;
+				}
 			}
 		}
+
+		return nullptr;
 	}
 }
 
