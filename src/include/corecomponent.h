1fc5c896e208e29d93bd0ea7e9492448bfd922ce
Michael Bethke <gibsonbethke@gmail.com>
2022-03-08 21:21:18 -0600

    Created CoreComponent Base Class

---
+++ src/include/corecomponent.h
@@ -0,0 +1,16 @@
+#ifndef CORECOMPONENT_H
+#define CORECOMPONENT_H
+
+class CoreComponent {
+public:
+	CoreComponent() = default;
+	CoreComponent(const CoreComponent&) = delete;
+	CoreComponent(CoreComponent&&) = delete;
+	CoreComponent& operator=(const CoreComponent&) = delete;
+	CoreComponent& operator=(CoreComponent&&) = delete;
+	virtual ~CoreComponent() = default;
+
+	virtual void Update() = 0;
+};
+
+#endif
